a79ca1cb06aaa05ea0da9af19d761ab66eacdaeb
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-24 16:01:04 +0500

    updated

--- app/Http/Controllers/Api/MessengerController.php
+++ app/Http/Controllers/Api/MessengerController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 
 class MessengerController extends Controller
 {
-    public function webhook() {
+    public function webhook(Request $request) {
+        $challenge = $request->input('hub_challenge');
+        $verifyToken = $request->input('hub_verify_token');
+        if ($verify_token == env('PAGE_ACCESS_TOKEN')) {
+            return response($challenge, 200);
+        }
         
+        return response('Forbidden', 403);
     }
 }
