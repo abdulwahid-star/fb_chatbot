fcf1ba84f96c19f203f8142db12d0d435df50016
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-24 16:10:56 +0500

    updated packages

--- app/Http/Controllers/Api/MessengerController.php
+++ app/Http/Controllers/Api/MessengerController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use pimax\FbBotApp;
+use pimax\Messages\message;
 
 class MessengerController extends Controller
 {
     public function webhook(Request $request) {
         $challenge = $request->input('hub_challenge');
         $verifyToken = $request->input('hub_verify_token');
-        if ($verify_token == env('PAGE_ACCESS_TOKEN')) {
+        if ($verifyToken == env('PAGE_ACCESS_TOKEN')) {
             return response($challenge, 200);
         }
         
