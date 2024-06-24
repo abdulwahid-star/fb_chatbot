783c0614660abb742d514a0630e81eb3982bc6cb
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-24 17:27:45 +0500

    updated

--- app/Http/Controllers/Api/MessengerController.php
+++ app/Http/Controllers/Api/MessengerController.php
@@ -12,10 +12,16 @@ class MessengerController extends Controller
     public function webhook(Request $request) {
         $challenge = $request->input('hub_challenge');
         $verifyToken = $request->input('hub_verify_token');
-        if ($verifyToken == env('PAGE_ACCESS_TOKEN')) {
-            return response($challenge, 200);
+        $mode = $request->input('hub_mode');
+
+        if ($mode && $verifyToken) {
+            if ($mode === 'subscribe' && $verifyToken === env('PAGE_ACCESS-TOKEN')) {
+                return response($challenge, 200);
+            } else {
+                return response('Forbidden', 403);
+            }
         }
-        
-        return response('Forbidden', 403);
+
+        return response('Bad Request', 400);
     }
 }
