810b676648f39e619746c869e6d1731804f1f375
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-24 17:57:03 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\MessengerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/webhook', [MessengerController::class, 'webhook']);
