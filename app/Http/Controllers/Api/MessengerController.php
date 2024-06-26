990805d670e3ceef0861f476f636cef1d35f3e1e
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-26 19:11:50 +0500

    updared

--- app/Http/Controllers/Api/MessengerController.php
+++ app/Http/Controllers/Api/MessengerController.php
@@ -15,25 +15,25 @@ class MessengerController extends Controller
             'message' => $request->all()
         ]);
 
-        $data = $request->all();
+        // $data = $request->all();
 
-        if ($data['object'] === 'page') {
-            foreach ($data['entry'] as $entry) {
-                foreach ($entry['changes'] as $change) {
-                    if ($change['field'] === 'comments' && $change['value']['item'] === 'comment') {
-                        $commentId = $change['value']['comment_id'];
-                        $pagepostId = $change['value']['page_post_id']; // Use page_post_id instead of post_id
-                        $message = 'Thank you for your comment!'; // Your reply message
+        // if ($data['object'] === 'page') {
+        //     foreach ($data['entry'] as $entry) {
+        //         foreach ($entry['changes'] as $change) {
+        //             if ($change['field'] === 'comments' && $change['value']['item'] === 'comment') {
+        //                 $commentId = $change['value']['comment_id'];
+        //                 $pagepostId = $change['value']['page_post_id']; // Use page_post_id instead of post_id
+        //                 $message = 'Thank you for your comment!'; // Your reply message
 
-                        $this->replyToComment($pagepostId, $commentId, $message);
-                    }
-                }
-            }
+        //                 $this->replyToComment($pagepostId, $commentId, $message);
+        //             }
+        //         }
+        //     }
 
-            return response('EVENT_RECEIVED', 200);
-        }
+        //     return response('EVENT_RECEIVED', 200);
+        // }
 
-        return response('Bad Request', 400);
+        // return response('Bad Request', 400);
     }
 
     private function replyToComment($pagepostId, $commentId, $message)
