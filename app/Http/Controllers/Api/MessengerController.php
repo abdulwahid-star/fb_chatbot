f356362f3274d50845119053e3ef50eafe48e73d
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-26 19:20:57 +0500

    updated

--- app/Http/Controllers/Api/MessengerController.php
+++ app/Http/Controllers/Api/MessengerController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 class MessengerController extends Controller
 {
     public function webhook(Request $request) {
-        response()->json([
+        return response()->json([
             'message' => $request->all()
         ]);
 
