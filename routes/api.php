5513345fdf03fb56f6b606bee2f80988c869f32e
Abdul Wahid <abdulwahidofficials@gmail.com>
2024-06-26 23:21:14 +0500

    updated

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MessengerController;
+use App\Http\Middleware\VerifyBot;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/webhook', [MessengerController::class, 'webhook'])->middleware(VerifyBot::class);
+
+Route::post('/webhook', [MessengerController::class, 'webhook']);
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\MessengerController;
-use App\Http\Middleware\VerifyBot;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/webhook', [MessengerController::class, 'webhook'])->middleware(VerifyBot::class);
-
-Route::post('/webhook', [MessengerController::class, 'webhook']);
